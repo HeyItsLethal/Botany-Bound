b1b232a781256cb92f7d18420dc192a3ab2fe5a5
Adam Grant <adam.grant2@dcmail.ca>
2024-02-14 16:41:27 -0500

    Added some core blocks to js file

---
+++ plugins/botany-bound/plugin.js
@@ -0,0 +1,24 @@
+wp.blocks.registerBlockStyle( 'core/paragraph', {
+    name: 'botany-paragraph',
+    label: 'Botany Paragraph',
+} );
+
+wp.blocks.registerBlockStyle( 'core/heading', {
+    name: 'botany-heading',
+    label: 'Botany Heading',
+} );
+
+wp.blocks.registerBlockStyle( 'core/quote', {
+    name: 'botany-quote',
+    label: 'Botany Quote',
+} );
+
+wp.blocks.registerBlockStyle( 'core/code', {
+    name: 'botany-code',
+    label: 'Botany Code',
+} );
+
+wp.blocks.registerBlockStyle( 'core/details', {
+    name: 'botany-details',
+    label: 'Botany Details',
+} );
